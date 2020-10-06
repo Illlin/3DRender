b3732a792077a9e1c354b16fa0b52b0fb2cd8e90
illin <IllinTori@gmail.com>
2020-10-06 10:57:14 +0100

    Added Octahedron distance function

--- objects.py
+++ objects.py
@@ -1,4 +1,4 @@
-import numpy as np
+import glm
 from functions import maths
 
 mag = maths.mag
@@ -9,7 +9,7 @@ mag = maths.mag
 class Point:
     # Point object, most basic 3d object
     def __init__(self, pos):
-        self.pos = np.array(pos)
+        self.pos = pos
 
     def distance_to(self, other):
         """
@@ -23,8 +23,8 @@ class Point:
 class Circle(Point):
     # Sphere really, but circle is simpler to spell
     def __init__(self, pos, radius):
-        self.pos = np.array(pos)
-        self.radius = np.double(radius)
+        self.pos = pos
+        self.radius = radius
         super().__init__(pos)
 
     def distance_to(self, point):
@@ -34,19 +34,55 @@ class Circle(Point):
 class Box(Point):
     # A box, of any dimension.
     def __init__(self, pos, size):
-        self.pos = np.array(pos)
-        self.size = np.array(size)
+        self.pos = pos
+        self.size = size
         super().__init__(pos)
 
     def distance_to(self, point):
         # Outwards distance.
-        d = abs(point) - self.size
+        d = abs(glm.vec3(point)-glm.vec3(self.pos)) - glm.vec3(self.size)
         inside = min(max(d[0], max(d[1], d[2])), 0.0)
-        out = mag(np.array([max(i,0) for i in d]))
+
+        # TODO check this out vv
+        out = mag([max(i,0) for i in d])
         return inside + out
 
 
-#class Torus()
+class Torus(Point):
+    # A torus
+    def __init__(self, pos, size):
+        super().__init__(pos)
+        self.pos = pos
+        self.size = size
+
+    def distance_to(self, point):
+        p = glm.vec3(point) - glm.vec3(self.pos)
+        q = glm.vec2(mag(glm.vec2(p.x, p.z))-self.size[0], p.y)
+        return mag(q) - self.size[1]
+
+
+class Octahedron(Point):
+    # A Octahedron
+    def __init__(self, pos, size):
+        super().__init__(pos)
+        self.pos = pos
+        self.size = size
+        self.magicnum = (1/3)**0.5
+
+    def distance_to(self, point):
+        pos = glm.vec3(point) - glm.vec3(self.pos)
+        return (sum([abs(x) for x in pos]) - self.size)*self.magicnum
+
+
+
+class Repetition():
+    def __init__(self, obj, c):
+        self.obj = obj
+        self.c = c
+
+    def distance_to(self, p):
+        q = (p % self.c)-(self.c*0.5)
+        return self.obj.distance_to(q)
 
 
 class Intersect(Point):
